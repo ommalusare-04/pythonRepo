cc37af6f9eb72d75e527e122e5183e9d2e29cdfd
Om Malusare <omalusare850@gmail.com>
2026-09-11 15:26:31 +0630

    commit

---
+++ daysweek.py
@@ -0,0 +1,17 @@
+day=int(input("enter the date:"))
+if day ==1 :
+    print(f"{day} is monday")
+elif day ==2:
+    print(f"{day} is tuesday")
+elif day ==3:
+    print(f"{day} is wednesday")
+elif day ==4:
+    print(f"{day} is thursday")
+elif day ==5:
+    print(f"{day} is friday")
+elif day ==6:
+    print(f"{day} is saturday")
+elif day ==7:
+    print(f"{day} is sunday")
+else:
+    print("date is invalid")
