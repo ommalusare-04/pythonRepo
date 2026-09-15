cca2150031a9ec00b07768be5a9176e567af70a1
Om Malusare <omalusare850@gmail.com>
2026-09-15 09:55:33 +0630

    commited

--- whileloop.py
+++ whileloop.py
@@ -1,3 +1,16 @@
-while True:
-    string=input("enter the string:")
-    print(string)
+# while True:
+#     string=input("enter the string:")
+#     print(string)
+
+correct_pass = "some_pass"
+
+not_found = True
+
+while not_found:
+
+    passw = input("Enter the pass: ")
+
+    if passw == correct_pass:
+        not_found = False
+
+print("Correct password")
